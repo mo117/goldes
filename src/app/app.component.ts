0fac4b9093de80875fc1b4e90123e2dad08ba297
mo117 <midohamed142@gmail.com>
2020-08-18 02:03:39 +0200

    tranlate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NgxSpinnerService } from "ngx-spinner";
 
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Component({
@@ -13,25 +14,34 @@ export class AppComponent {
   title = 'goldes';
   loader: boolean;
   constructor(
-    // private cookieService: CookieService,
+    private cookieService: CookieService,
     private translateService: TranslateService,
     private spinner: NgxSpinnerService
-) {
+  ) {
     translateService.setDefaultLang('en');
     this.loader = true
   }
   changeLang(lang: string) {
     this.translateService.use(lang);
     this.translateService.setDefaultLang(lang);
+    this.cookieService.set('lang', lang)
   }
   ngOnInit() {
+    if (this.cookieService.get('lang') == 'ar') {
+      this.translateService.use('ar');
+      this.translateService.setDefaultLang('ar');
+    }
+    else {
+      this.translateService.use('en');
+      this.translateService.setDefaultLang('en');
+    }
     this.spinner.show();
- 
+
     setTimeout(() => {
       this.spinner.hide();
       this.loader = false
     }, 2000);
 
-    
+
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HostListener } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Component({
@@ -12,43 +13,52 @@ export class NavbarComponent implements OnInit {
   IsArbic: boolean;
   IsEnglish: boolean;
   constructor(
-    private translateService: TranslateService
-  ) { }
-
-  ngOnInit(): void {
+    private translateService: TranslateService,
+    private cookieService: CookieService,
+  ) {
     this.IsArbic = true;
     this.IsEnglish = false;
   }
-  navbarOpen:boolean = false;
 
-  toggleNavbar(){
+  ngOnInit(): void {
+    if (this.cookieService.get('lang') == 'en') {
+      this.IsArbic = true;
+      this.IsEnglish = false;
+    }
+    else {
+      this.IsArbic = false;
+      this.IsEnglish = true;
+    }
+  }
 
-    this.navbarOpen = !this.navbarOpen
-  };
-
-  
   @HostListener('window:scroll', ['$event'])
 
   onWindowScroll(e) {
-      let element = document.querySelector('.navbar');
-      if (window.pageYOffset > element.clientHeight) {
-        element.classList.add('navbar-inverse');
-      } else {
-        element.classList.remove('navbar-inverse');
-      }
-
+    let element = document.querySelector('.navbar');
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbar-inverse');
+    } else {
+      element.classList.remove('navbar-inverse');
     }
-    changeLang(lang: string) {
-      if(lang == 'ar') {
-        this.IsArbic = false;
-        this.IsEnglish = true;
-      } 
-      else {
-        this.IsArbic = true;
-        this.IsEnglish = false;
-      }
-      this.translateService.use(lang);
-      this.translateService.setDefaultLang(lang);
+
+  }
+  changeLang(lang: string) {
+    if (lang == 'ar') {
+      this.IsArbic = false;
+      this.IsEnglish = true;
+    }
+    else {
+      this.IsArbic = true;
+      this.IsEnglish = false;
+    }
+    this.translateService.use(lang);
+    this.translateService.setDefaultLang(lang);
+    this.cookieService.set('lang', lang)
+
+  }
+  navClick() {
+    let element = document.querySelector('.navbar-collapse');
+    element.classList.remove('show');
   }
 
 
